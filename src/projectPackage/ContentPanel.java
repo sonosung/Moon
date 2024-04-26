a2368ac6bdc467d985e94c9dca1d7d880e09499c
wjrls <wjrls@naver.com>
2024-04-26 17:42:50 +0900

    Jlist 예제 업데이트

--- src/projectPackage/ContentPanel.java
+++ src/projectPackage/ContentPanel.java
@@ -1,9 +1,20 @@
 package projectPackage;
 
+import java.awt.AWTEvent;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Rectangle;
+import java.util.Vector;
 
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.ListCellRenderer;
+import javax.swing.ListSelectionModel;
+import javax.swing.SwingConstants;
+import javax.swing.JLabel;
+import javax.swing.JList;
 
 public class ContentPanel extends JPanel {
 
@@ -16,13 +27,68 @@ public class ContentPanel extends JPanel {
 	public ContentPanel(MainFrame mainFrame) {
 		
 		this.mainFrame = mainFrame;
-		
-		setLayout(null);
 		this.setSize(1280,800-150);
 		this.setPreferredSize(new Dimension(1280,800-150));
 		this.setBackground(Color.green);
 		
+		Vector<String> testvec = new Vector<String>();
+		testvec.add("100");
+		testvec.add("002");
+		testvec.add("300");
+		testvec.add("004");
+		testvec.add("500");
+		testvec.add("006");
+		testvec.add("7");
+		testvec.add("8");
+		testvec.add("9");
+		testvec.add("10");
+		testvec.add("11");
+		setLayout(null);
+		
+		
+		JList<String> list = new JList<String>(testvec);
+		
+		
+		
+		list.setCellRenderer(new MyCellRenderer());
+		list.setBounds(192, 199, 271, 222);
+		
+		list.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		
+		JScrollPane scrollPane = new JScrollPane(list);
+		scrollPane.setBounds(0, 252, 258, 133);
+		//scrollPane.setPreferredSize(new Dimension(200, 150));
+		
+		
+		add(scrollPane);
 		this.setVisible(false);		
 	}
+	
+	 class MyCellRenderer extends JLabel implements ListCellRenderer<Object> {
+	        MyCellRenderer() {
+	            setOpaque(true);
+	        }
 
+	        public Component getListCellRendererComponent(JList<?> list, Object value, int index, boolean isSelected, boolean cellHasFocus) {
+	            setText(value.toString());
+
+	            // Customize size of each list item
+	            setPreferredSize(new Dimension(50, 50)); // Set preferred width and height for each item
+	            setHorizontalAlignment(SwingConstants.RIGHT);
+	            
+	            Font font = getFont();
+	            setFont(font.deriveFont(20f));
+
+	            // Set background and foreground colors based on selection
+	            if (isSelected) {
+	                setBackground(list.getSelectionBackground());
+	                setForeground(list.getSelectionForeground());
+	            } else {
+	                setBackground(list.getBackground());
+	                setForeground(list.getForeground());
+	            }
+
+	            return this;
+	        }
+	    }
 }
