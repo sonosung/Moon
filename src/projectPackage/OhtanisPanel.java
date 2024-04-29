7b9d7da10ac45ad234122575be40b30634f87be7
21-manic-008g <21-manic-008g@21-manic-008>
2024-04-29 15:44:56 +0900

    bgcolor 변경

--- src/projectPackage/OhtanisPanel.java
+++ src/projectPackage/OhtanisPanel.java
@@ -36,7 +36,7 @@ public class OhtanisPanel extends JPanel {
 		setLayout(null);
 		this.setSize(1280, 800 - 150);
 		this.setPreferredSize(new Dimension(1280, 800 - 150));
-		this.setBackground(Color.gray);
+		this.setBackground(new Color(0, 0, 0));
 
 		try {
 
@@ -48,35 +48,35 @@ public class OhtanisPanel extends JPanel {
 			JButton btnNewButton_1 = new JButton("");		
 			btnNewButton_1.addActionListener(new crimePopupAction());
 			btnNewButton_1.setIcon(new ImageIcon(OhtanisPanel.class.getResource("/image/ohtani/crimetown.jpg")));
-			btnNewButton_1.setBounds(88, 336, 150, 214);
+			btnNewButton_1.setBounds(88, 326, 150, 214);
 			add(btnNewButton_1);
 
 			stuntPopup = new StuntPopup();
 			JButton btnNewButton_2 = new JButton("");
 			btnNewButton_2.addActionListener(new stuntPopupAction());
 			btnNewButton_2.setIcon(new ImageIcon(OhtanisPanel.class.getResource("/image/ohtani/stuntman.jpg")));
-			btnNewButton_2.setBounds(326, 336, 150, 214);
+			btnNewButton_2.setBounds(326, 326, 150, 214);
 			add(btnNewButton_2);
 
 			kungfuPopup = new KungfuPopup();
 			JButton btnNewButton_3 = new JButton("");
 			btnNewButton_3.addActionListener(new kungfuPopupAction());
 			btnNewButton_3.setIcon(new ImageIcon(OhtanisPanel.class.getResource("/image/ohtani/kungfupanda.jpg")));
-			btnNewButton_3.setBounds(560, 336, 150, 214);
+			btnNewButton_3.setBounds(560, 326, 150, 214);
 			add(btnNewButton_3);
 
 			chalPopup = new ChalPopup();
 			JButton btnNewButton_4 = new JButton("");
 			btnNewButton_4.addActionListener(new chalPopupAction());
 			btnNewButton_4.setIcon(new ImageIcon(OhtanisPanel.class.getResource("/image/ohtani/challengers.jpg")));
-			btnNewButton_4.setBounds(802, 336, 150, 214);
+			btnNewButton_4.setBounds(802, 326, 150, 214);
 			add(btnNewButton_4);
 			
 			monPopup = new MonPopup();
 			JButton btnNewButton_5 = new JButton("");
 			btnNewButton_5.addActionListener(new monPopupAction());
 			btnNewButton_5.setIcon(new ImageIcon(OhtanisPanel.class.getResource("/image/ohtani/monsterfriends.jpg")));
-			btnNewButton_5.setBounds(1040, 336, 150, 214);
+			btnNewButton_5.setBounds(1040, 326, 150, 214);
 			add(btnNewButton_5);
 			
 
@@ -86,53 +86,63 @@ public class OhtanisPanel extends JPanel {
 			add(btnNewButton_0);
 
 			JLabel label = new JLabel("1. 범죄도시4");
+			label.setForeground(new Color(255, 255, 255));
 			label.setFont(new Font("나눔고딕", Font.BOLD, 18));
-			label.setBounds(111, 565, 100, 22);
+			label.setBounds(111, 550, 100, 22);
 			add(label);
 
 			JLabel label_1 = new JLabel("2. 스턴트맨");
+			label_1.setForeground(new Color(255, 255, 255));
 			label_1.setFont(new Font("나눔고딕", Font.BOLD, 18));
-			label_1.setBounds(351, 565, 100, 22);
+			label_1.setBounds(351, 550, 100, 22);
 			add(label_1);
 
 			JLabel label_1_1 = new JLabel("3. 쿵푸팬더4");
+			label_1_1.setForeground(new Color(255, 255, 255));
 			label_1_1.setFont(new Font("나눔고딕", Font.BOLD, 18));
-			label_1_1.setBounds(585, 565, 100, 22);
+			label_1_1.setBounds(585, 550, 100, 22);
 			add(label_1_1);
 
 			JLabel label_1_1_1 = new JLabel("5. 몬스터 프렌즈");
+			label_1_1_1.setForeground(new Color(255, 255, 255));
 			label_1_1_1.setFont(new Font("나눔고딕", Font.BOLD, 18));
-			label_1_1_1.setBounds(1046, 565, 139, 22);
+			label_1_1_1.setBounds(1046, 550, 139, 22);
 			add(label_1_1_1);
 
 			JLabel label_1_1_1_1 = new JLabel("4. 챌린저스");
+			label_1_1_1_1.setForeground(new Color(255, 255, 255));
 			label_1_1_1_1.setFont(new Font("나눔고딕", Font.BOLD, 18));
-			label_1_1_1_1.setBounds(827, 565, 100, 22);
+			label_1_1_1_1.setBounds(827, 550, 100, 22);
 			add(label_1_1_1_1);
 
 			JLabel label_2 = new JLabel("예매율 91.2%");
+			label_2.setForeground(new Color(255, 255, 255));
 			label_2.setFont(new Font("나눔고딕", Font.BOLD, 12));
-			label_2.setBounds(130, 588, 85, 22);
+			label_2.setBounds(130, 573, 85, 22);
 			add(label_2);
 
 			JLabel label_2_1 = new JLabel("예매율 1.4%");
+			label_2_1.setForeground(new Color(255, 255, 255));
 			label_2_1.setFont(new Font("나눔고딕", Font.BOLD, 12));
-			label_2_1.setBounds(370, 588, 85, 22);
+			label_2_1.setBounds(370, 573, 85, 22);
 			add(label_2_1);
 
 			JLabel label_2_1_1 = new JLabel("예매율 1.3%");
+			label_2_1_1.setForeground(new Color(255, 255, 255));
 			label_2_1_1.setFont(new Font("나눔고딕", Font.BOLD, 12));
-			label_2_1_1.setBounds(609, 588, 85, 22);
+			label_2_1_1.setBounds(609, 573, 85, 22);
 			add(label_2_1_1);
 
 			JLabel label_2_1_1_1 = new JLabel("예매율 1.2%");
+			label_2_1_1_1.setForeground(new Color(255, 255, 255));
 			label_2_1_1_1.setFont(new Font("나눔고딕", Font.BOLD, 12));
-			label_2_1_1_1.setBounds(847, 588, 85, 22);
+			label_2_1_1_1.setBounds(847, 573, 85, 22);
 			add(label_2_1_1_1);
 
 			JLabel label_2_1_1_1_1 = new JLabel("예매율 1.2%");
+			label_2_1_1_1_1.setForeground(new Color(255, 255, 255));
 			label_2_1_1_1_1.setFont(new Font("나눔고딕", Font.BOLD, 12));
-			label_2_1_1_1_1.setBounds(1084, 588, 85, 22);
+			label_2_1_1_1_1.setBounds(1084, 573, 85, 22);
 			add(label_2_1_1_1_1);
 
 		} catch (Exception e) {
